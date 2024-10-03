7681e9286219ce6eb39cd35c037d92127d4f339c
Anton Ostapchenko <OstapchenkoTosha@yandex.ru>
2024-10-03 20:58:48 +0700

    Создали модель товар, в которой имеются поля название, описание, цена, количество, дата добавления

--- home_app/models.py
+++ home_app/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Client(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=100)
     email = models.EmailField()
     phone_number = models.CharField(max_length=20)
     address = models.TextField()
@@ -9,4 +9,13 @@ class Client(models.Model):
 
     def __str__(self):
         return self.name
-# Create your models here.
+
+class Product(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.EmailField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.PositiveIntegerField()
+    added_date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
