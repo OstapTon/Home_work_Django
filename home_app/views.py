00a638ec043ef5be6b0a9a5e97b00109203de4ed
Anton Ostapchenko <OstapchenkoTosha@yandex.ru>
2024-10-03 22:01:51 +0700

    Создали функции для обработки CRUD-запросов для каждой модели

--- home_app/views.py
+++ home_app/views.py
@@ -1,27 +1,133 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from random import randint
-import logging
+# views.py
+from django.shortcuts import render, get_object_or_404, redirect
+from home_app.models import Client, Product, Order
+from home_app.forms import ClientForm, ProductForm, OrderForm
 
-logger = logging.getLogger(__name__)
+# CRUD для клиента
 
+# Create (Client)
+def create_client(request):
+    if request.method == 'POST':
+        form = ClientForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('client_list')  # Перенаправление на список клиентов
+    else:
+        form = ClientForm()
+    return render(request, 'create_client.html', {'form': form})
 
-def text(title, text):
-    return f'<h1>Первый сайт Django</h1>' \
-        f'<h2>{title}</h2>' \
-        f'<p>{text}</p>'
+# Read (Client list)
+def client_list(request):
+    clients = Client.objects.all()
+    return render(request, 'client_list.html', {'clients': clients})
 
+# Read (Client details)
+def client_detail(request, pk):
+    client = get_object_or_404(Client, pk=pk)
+    return render(request, 'client_detail.html', {'client': client})
 
-def main_page(request):
-    title = 'Главная страница сайта'
-    body_text = 'Текст главной страницы сайта, с наполнением'
-    logger.info(f'Page "main" is open')
-    return HttpResponse(text(title, body_text))
+# Update (Client)
+def update_client(request, pk):
+    client = get_object_or_404(Client, pk=pk)
+    if request.method == 'POST':
+        form = ClientForm(request.POST, instance=client)
+        if form.is_valid():
+            form.save()
+            return redirect('client_detail', pk=pk)
+    else:
+        form = ClientForm(instance=client)
+    return render(request, 'update_client.html', {'form': form})
 
+# Delete (Client)
+def delete_client(request, pk):
+    client = get_object_or_404(Client, pk=pk)
+    if request.method == 'POST':
+        client.delete()
+        return redirect('client_list')
+    return render(request, 'delete_client.html', {'client': client})
 
-def about_me(request):
-    title = 'О себе'
-    body_text = 'Информация...<br>' \
-                'Ок'
-    logger.info(f'Page "about_me" is open')
-    return HttpResponse(text(title, body_text))
+# CRUD для товара
+
+# Create (Product)
+def create_product(request):
+    if request.method == 'POST':
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('product_list')
+    else:
+        form = ProductForm()
+    return render(request, 'create_product.html', {'form': form})
+
+# Read (Product list)
+def product_list(request):
+    products = Product.objects.all()
+    return render(request, 'product_list.html', {'products': products})
+
+# Read (Product details)
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    return render(request, 'product_detail.html', {'product': product})
+
+# Update (Product)
+def update_product(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    if request.method == 'POST':
+        form = ProductForm(request.POST, instance=product)
+        if form.is_valid():
+            form.save()
+            return redirect('product_detail', pk=pk)
+    else:
+        form = ProductForm(instance=product)
+    return render(request, 'update_product.html', {'form': form})
+
+# Delete (Product)
+def delete_product(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    if request.method == 'POST':
+        product.delete()
+        return redirect('product_list')
+    return render(request, 'delete_product.html', {'product': product})
+
+# CRUD для заказа
+
+# Create (Order)
+def create_order(request):
+    if request.method == 'POST':
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('order_list')
+    else:
+        form = OrderForm()
+    return render(request, 'create_order.html', {'form': form})
+
+# Read (Order list)
+def order_list(request):
+    orders = Order.objects.all()
+    return render(request, 'order_list.html', {'orders': orders})
+
+# Read (Order details)
+def order_detail(request, pk):
+    order = get_object_or_404(Order, pk=pk)
+    return render(request, 'order_detail.html', {'order': order})
+
+# Update (Order)
+def update_order(request, pk):
+    order = get_object_or_404(Order, pk=pk)
+    if request.method == 'POST':
+        form = OrderForm(request.POST, instance=order)
+        if form.is_valid():
+            form.save()
+            return redirect('order_detail', pk=pk)
+    else:
+        form = OrderForm(instance=order)
+    return render(request, 'update_order.html', {'form': form})
+
+# Delete (Order)
+def delete_order(request, pk):
+    order = get_object_or_404(Order, pk=pk)
+    if request.method == 'POST':
+        order.delete()
+        return redirect('order_list')
+    return render(request, 'delete_order.html', {'order': order})
