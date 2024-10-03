0ec070d0b20903c6fba6cb3f85cd3cc0f9b23e58
Anton Ostapchenko <OstapchenkoTosha@yandex.ru>
2024-10-03 22:02:24 +0700

    Настройка URL-маршрутов

--- home_app/urls.py
+++ home_app/urls.py
@@ -14,10 +14,34 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# urls.py
 from django.urls import path
-from . import views
+from .views import (
+    create_client, client_list, client_detail, update_client, delete_client,
+    create_product, product_list, product_detail, update_product, delete_product,
+    create_order, order_list, order_detail, update_order, delete_order
+)
 
 urlpatterns = [
-    path('', views.main_page, name='Main_page'),
-    path('about/', views.about_me, name='About_me'),
+    # Client URLs
+    path('clients/', client_list, name='client_list'),
+    path('client/new/', create_client, name='create_client'),
+    path('client/<int:pk>/', client_detail, name='client_detail'),
+    path('client/<int:pk>/edit/', update_client, name='update_client'),
+    path('client/<int:pk>/delete/', delete_client, name='delete_client'),
+
+    # Product URLs
+    path('products/', product_list, name='product_list'),
+    path('product/new/', create_product, name='create_product'),
+    path('product/<int:pk>/', product_detail, name='product_detail'),
+    path('product/<int:pk>/edit/', update_product, name='update_product'),
+    path('product/<int:pk>/delete/', delete_product, name='delete_product'),
+
+    # Order URLs
+    path('orders/', order_list, name='order_list'),
+    path('order/new/', create_order, name='create_order'),
+    path('order/<int:pk>/', order_detail, name='order_detail'),
+    path('order/<int:pk>/edit/', update_order, name='update_order'),
+    path('order/<int:pk>/delete/', delete_order, name='delete_order'),
 ]
+
