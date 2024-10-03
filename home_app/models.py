8f2193e6b4d3e463c30c7b5c405c907303832aa0
Anton Ostapchenko <OstapchenkoTosha@yandex.ru>
2024-10-03 19:23:57 +0700

    Создали модель клиент, в которой имеются поля имя, электронная почта, номер телефона, адрес, дата, регистрации клиента

--- home_app/models.py
+++ home_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+class Client(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=20)
+    address = models.TextField()
+    registration_date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
 # Create your models here.
